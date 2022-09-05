0231cb476fe9d60ea9ffb54c2b9b127f589b228e
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-09-05 13:21:56 +0800

    Added P15
    Added P15

---
+++ P15_ShaderAbstractionInOpenGL/src/Shader.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+struct ShaderProgramSource
+{
+	std::string VertexSource;
+	std::string FragmentSource;
+};
+
+class Shader
+{
+private:
+	std::string m_FilePath;
+	unsigned int m_RendererID;
+	std::unordered_map<std::string, int> m_UniformLocationCache;
+	// caching for uniforms
+public:
+	Shader(const std::string& filepath);
+	~Shader();
+
+	void bind() const;
+	void unBind() const;
+
+	// Set uniforms
+	void setUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+private:
+	ShaderProgramSource parseShader(const std::string& filepath);
+	unsigned int compileShader(unsigned int type, const std::string& source);
+	unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader);
+	int getUniformLocation(const std::string& name);
+};
+
---
+++ P15_ShaderAbstractionInOpenGL/src/VertexArray.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+
+class VertexArray
+{
+private:
+	unsigned int m_RendererID;
+
+public:
+	VertexArray();
+	~VertexArray();
+
+	void addBuffer(const VertexBuffer& vb, const VertexBufferLayout& layout);
+
+	void bind() const;
+	void unBind() const;
+};
