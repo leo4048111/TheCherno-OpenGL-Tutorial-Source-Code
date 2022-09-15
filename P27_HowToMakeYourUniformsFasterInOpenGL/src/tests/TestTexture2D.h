76448d4538af5f568816ae22f190b143686bd1fe
Potato5K <74029782+leo4048111@users.noreply.github.com>
2022-09-15 17:14:48 +0800

    Added P27
    Added P27

---
+++ P27_HowToMakeYourUniformsFasterInOpenGL/src/tests/TestTexture2D.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Test.h"
+
+#include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
+#include "Texture.h"
+
+#include <memory>
+
+namespace test
+{
+	class TestTexture2D : public Test
+	{
+	public:
+		TestTexture2D();
+		~TestTexture2D();
+
+		void onUpdate(float deltaTime) override;
+		void onRender() override;
+		void onImGuiRender() override;
+	private:
+		std::unique_ptr<VertexArray> m_VAO;
+		std::unique_ptr<VertexBuffer>m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<Texture> m_Texture;
+
+		glm::mat4 m_Proj, m_View;
+		glm::vec3 m_TranslationA, m_TranslationB;
+	};
+}
